3e28aacb44a0e1a347dc944558545d5344558fc3
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2021-10-14 09:45:05 +0200

    move services version in application.conf

--- src/main/scala/it/pagopa/pdnd/interop/uservice/agreementprocess/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/agreementprocess/common/system/ApplicationConfiguration.scala
@@ -9,28 +9,12 @@ object ApplicationConfiguration {
     config.getInt("uservice-agreement-process.port")
   }
 
-  def agreementManagementURL: String = {
-    val agreementMgmtURL: String = config.getString("services.agreement-management")
-    s"$agreementMgmtURL/pdnd-interop-uservice-agreement-management/0.0.1"
-//    s"https://gateway.interop.pdnd.dev/pdnd-interop-uservice-agreement-management/0.0.1"
-  }
+  def agreementManagementURL: String = config.getString("services.agreement-management")
 
-  def catalogManagementURL: String = {
-    val catalogMgmtURL: String = config.getString("services.catalog-management")
-    s"$catalogMgmtURL/pdnd-interop-uservice-catalog-management/0.0.1"
-//    s"https://gateway.interop.pdnd.dev/pdnd-interop-uservice-catalog-management/0.0.1"
-  }
+  def catalogManagementURL: String = config.getString("services.catalog-management")
 
-  def partyManagementURL: String = {
-    val partyMgmtURL: String = config.getString("services.party-management")
-    s"$partyMgmtURL/pdnd-interop-uservice-party-management/0.0.1"
-//    s"https://gateway.interop.pdnd.dev/pdnd-interop-uservice-catalog-management/0.0.1"
-  }
+  def partyManagementURL: String = config.getString("services.party-management")
 
-  def attributeRegistryManagementURL: String = {
-    val attributeRegistryMgmtURL: String = config.getString("services.attribute-registry-management")
-    s"$attributeRegistryMgmtURL/pdnd-interop-uservice-attribute-registry-management/0.0.1"
-//    s"https://gateway.interop.pdnd.dev/pdnd-interop-uservice-attribute-registry-management/0.0.1"
-  }
+  def attributeRegistryManagementURL: String = config.getString("services.attribute-registry-management")
 
 }
