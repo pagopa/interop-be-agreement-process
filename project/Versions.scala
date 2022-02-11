a40a3890eb7e79683d1e0e719e7d3045f4d10a97
Antonio Gelameris <toniogela89@gmail.com>
2022-02-11 17:27:39 +0100

    Updating dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -27,5 +27,5 @@ object PDNDVersions {
   lazy val attributeRegistryManagementVersion = "0.1.1-SNAPSHOT"
   lazy val catalogManagementVersion           = "0.1.1-SNAPSHOT"
   lazy val commonsVersion                     = "0.1.1-SNAPSHOT"
-  lazy val partyManagementVersion             = "0.1.1-SNAPSHOT"
+  lazy val partyManagementVersion             = "0.1.3-SNAPSHOT"
 }
