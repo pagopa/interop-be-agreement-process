2701f0f7392e5369da234a07f5211f33c764397a
galales <25105748+galales@users.noreply.github.com>
2022-04-27 14:54:39 +0200

    pin-1382: Verify the audience configuration is set

--- src/main/scala/it/pagopa/interop/agreementprocess/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/agreementprocess/common/system/ApplicationConfiguration.scala
@@ -3,16 +3,18 @@ package it.pagopa.interop.agreementprocess.common.system
 import com.typesafe.config.{Config, ConfigFactory}
 
 object ApplicationConfiguration {
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("agreement-process.port")
+  val serverPort: Int = config.getInt("agreement-process.port")
 
-  lazy val agreementManagementURL: String         = config.getString("agreement-process.services.agreement-management")
-  lazy val catalogManagementURL: String           = config.getString("agreement-process.services.catalog-management")
-  lazy val partyManagementURL: String             = config.getString("agreement-process.services.party-management")
-  lazy val attributeRegistryManagementURL: String =
+  val agreementManagementURL: String         = config.getString("agreement-process.services.agreement-management")
+  val catalogManagementURL: String           = config.getString("agreement-process.services.catalog-management")
+  val partyManagementURL: String             = config.getString("agreement-process.services.party-management")
+  val attributeRegistryManagementURL: String =
     config.getString("agreement-process.services.attribute-registry-management")
-  lazy val authorizationManagementURL: String = config.getString("agreement-process.services.authorization-management")
+  val authorizationManagementURL: String = config.getString("agreement-process.services.authorization-management")
 
-  lazy val jwtAudience: Set[String] = config.getString("agreement-process.jwt.audience").split(",").toSet
+  val jwtAudience: Set[String] = config.getString("agreement-process.jwt.audience").split(",").toSet
+
+  require(jwtAudience.nonEmpty, "Cannot have an empty audience")
 }
