a6779f38a4ff7a4ad903637fbd5fda30e7af6e4a
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-04-28 09:40:22 +0200

    pin-1382: Comma separated strings for accepted audiences instead of single env vars (#116)

--- src/main/scala/it/pagopa/interop/agreementprocess/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/agreementprocess/common/system/ApplicationConfiguration.scala
@@ -2,19 +2,19 @@ package it.pagopa.interop.agreementprocess.common.system
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.jdk.CollectionConverters.ListHasAsScala
-
 object ApplicationConfiguration {
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("agreement-process.port")
+  val serverPort: Int = config.getInt("agreement-process.port")
 
-  lazy val agreementManagementURL: String         = config.getString("agreement-process.services.agreement-management")
-  lazy val catalogManagementURL: String           = config.getString("agreement-process.services.catalog-management")
-  lazy val partyManagementURL: String             = config.getString("agreement-process.services.party-management")
-  lazy val attributeRegistryManagementURL: String =
+  val agreementManagementURL: String         = config.getString("agreement-process.services.agreement-management")
+  val catalogManagementURL: String           = config.getString("agreement-process.services.catalog-management")
+  val partyManagementURL: String             = config.getString("agreement-process.services.party-management")
+  val attributeRegistryManagementURL: String =
     config.getString("agreement-process.services.attribute-registry-management")
-  lazy val authorizationManagementURL: String = config.getString("agreement-process.services.authorization-management")
+  val authorizationManagementURL: String     = config.getString("agreement-process.services.authorization-management")
 
-  lazy val jwtAudience: Set[String] = config.getStringList("agreement-process.jwt.audience").asScala.toSet
+  val jwtAudience: Set[String] = config.getString("agreement-process.jwt.audience").split(",").toSet.filter(_.nonEmpty)
+
+  require(jwtAudience.nonEmpty, "Audience cannot be empty")
 }
