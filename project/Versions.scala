d0e46e28a3707429addbbaf2a0b6c996e5fe80d2
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2022-10-24 09:39:03 +0200

    Revert "update libs for tag 1.0.6"
    This reverts commit 0339ddace2eb8b8bddcc8b85e224aa1bf3a633b5.

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,12 +20,12 @@ object Versions {
 }
 
 object PagopaVersion {
-  lazy val agreementManagementVersion         = "1.0.4"
-  lazy val attributeRegistryManagementVersion = "1.0.4"
-  lazy val authorizationManagementVersion     = "1.0.6"
-  lazy val catalogManagementVersion           = "1.0.5"
-  lazy val commonsVersion                     = "1.0.8"
-  lazy val partyManagementVersion             = "1.0.1"
-  lazy val tenantManagementVersion            = "1.0.1"
-  lazy val userRegistryManagementVersion      = "1.0.1"
+  lazy val agreementManagementVersion         = "1.0.x-SNAPSHOT"
+  lazy val attributeRegistryManagementVersion = "1.0.x-SNAPSHOT"
+  lazy val authorizationManagementVersion     = "1.0.x-SNAPSHOT"
+  lazy val catalogManagementVersion           = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion                     = "1.0.x-SNAPSHOT"
+  lazy val partyManagementVersion             = "1.0.x-SNAPSHOT"
+  lazy val tenantManagementVersion            = "1.0.x-SNAPSHOT"
+  lazy val userRegistryManagementVersion      = "1.0.x-SNAPSHOT"
 }
