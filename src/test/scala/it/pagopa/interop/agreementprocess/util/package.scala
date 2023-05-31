db384490040d8a7c62a24f0ac190f58ab2e0de32
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2023-05-31 11:20:21 +0200

    Added support role to agreement process (#230)

--- src/test/scala/it/pagopa/interop/agreementprocess/util/package.scala
+++ src/test/scala/it/pagopa/interop/agreementprocess/util/package.scala
@@ -1,9 +1,9 @@
 package it.pagopa.interop.agreementprocess
 
-import it.pagopa.interop.commons.jwt.{ADMIN_ROLE, API_ROLE, INTERNAL_ROLE, M2M_ROLE, SECURITY_ROLE}
+import it.pagopa.interop.commons.jwt._
 
 package object util {
 
-  final val existingRoles: Seq[String] = Seq(ADMIN_ROLE, SECURITY_ROLE, API_ROLE, M2M_ROLE, INTERNAL_ROLE)
+  final val existingRoles: Seq[String] = Seq(ADMIN_ROLE, SECURITY_ROLE, API_ROLE, M2M_ROLE, INTERNAL_ROLE,SUPPORT_ROLE)
 
 }
