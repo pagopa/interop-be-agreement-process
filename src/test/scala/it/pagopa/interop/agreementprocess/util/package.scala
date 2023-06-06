fb88c8162f940272ac6a94b04392b4c4f496d62f
Roberto Gregnanin <gregnaninroberto@gmail.com>
2023-06-06 11:47:03 +0200

    Deleted validateEServiceDescriptorStatus and formatted code

--- src/test/scala/it/pagopa/interop/agreementprocess/util/package.scala
+++ src/test/scala/it/pagopa/interop/agreementprocess/util/package.scala
@@ -4,6 +4,6 @@ import it.pagopa.interop.commons.jwt._
 
 package object util {
 
-  final val existingRoles: Seq[String] = Seq(ADMIN_ROLE, SECURITY_ROLE, API_ROLE, M2M_ROLE, INTERNAL_ROLE,SUPPORT_ROLE)
+  final val existingRoles: Seq[String] = Seq(ADMIN_ROLE, SECURITY_ROLE, API_ROLE, M2M_ROLE, INTERNAL_ROLE, SUPPORT_ROLE)
 
 }
