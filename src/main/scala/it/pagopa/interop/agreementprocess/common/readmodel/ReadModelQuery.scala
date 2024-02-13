3ed7b155d30b9f7e6c1938e3f8e8517e68eb753a
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2024-02-13 16:29:04 +0100

    PIN-4557: Safer mongodb regex filters (#256)

--- src/main/scala/it/pagopa/interop/agreementprocess/common/readmodel/ReadModelQuery.scala
+++ src/main/scala/it/pagopa/interop/agreementprocess/common/readmodel/ReadModelQuery.scala
@@ -1,5 +1,12 @@
 package it.pagopa.interop.agreementprocess.common.readmodel
 
+import org.mongodb.scala.bson.conversions.Bson
+import org.mongodb.scala.model.Filters
+
 trait ReadModelQuery {
   def mapToVarArgs[A, B](l: Seq[A])(f: Seq[A] => B): Option[B] = Option.when(l.nonEmpty)(f(l))
+
+  def escape(str: String): String = str.replaceAll("([.*+?^${}()|\\[\\]\\\\])", "\\\\$1")
+  def safeRegex(fieldName: String, pattern: String, options: String): Bson =
+    Filters.regex(fieldName, escape(pattern), options)
 }
